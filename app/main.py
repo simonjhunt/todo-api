89018a05e4a615bed436c4053914160c7659d3c3
Simon Hunt <simon.hunt@buildingim.com>
2025-12-15 10:27:57 +0000

    refactor: move database to its own directory

--- app/main.py
+++ app/main.py
@@ -41,7 +41,7 @@ class ToDoGet(ToDoDB):
     pass
 
 
-sqlite_file_name = "app/database/database.db"
+sqlite_file_name = "data/database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
