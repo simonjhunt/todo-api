f96737154665a503d2c0d472ee16462e04a3c79b
Simon Hunt <simon.hunt@buildingim.com>
2025-12-16 11:18:36 +0000

    move pytest fixtures to conftest.py
    this declutters the tests a bit

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,36 +1,10 @@
 # tests/test_endpoints.py
-import pytest
 from fastapi import status
 
 from app.schemas.todo_get import ToDoGet
 
 from fastapi.testclient import TestClient
-from sqlmodel import Session, SQLModel, create_engine
-from sqlmodel.pool import StaticPool
 from app.main import app
-from app.db.session import get_session
-
-
-@pytest.fixture(name="session")
-def session_fixture():
-    engine = create_engine(
-        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
-    )
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
-        yield session
-
-
-@pytest.fixture(name="client")
-def client_fixture(session: Session):
-    def get_session_override():
-        return session
-
-    app.dependency_overrides[get_session] = get_session_override
-
-    client = TestClient(app)
-    yield client
-    app.dependency_overrides.clear()
 
 
 # -----------------------------------------------------------------
